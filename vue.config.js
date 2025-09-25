8c16288df1d8a28cc21c303f9fc425f8c27af567
nealxmw <nealxmw@qq.com>
2025-09-25 09:45:56 +0800

    默认标题更改

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-  open: true,          // 自动打开浏览器
-  host: 'localhost',   // 指定主机
-  port: 8080,          // 指定端口
-  https: false         // 启用 https
-}
+    open: true,          // 自动打开浏览器
+    host: 'localhost',   // 指定主机
+    port: 8080,          // 指定端口
+    https: false         // 启用 https
+  },
+  chainWebpack(config) {
+    config.plugin('html').tap(args => {
+      args[0].title = 'nealxmw';  // 在此设置页面标题
+      return args;
+    });
+  }
 })
